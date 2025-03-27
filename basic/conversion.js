1479a59a8b4285056601e0e9147967b7ec869fc5
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-27 14:40:19 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,35 @@
+let age="34"
+
+console.log(typeof(age));
+
+
+// conversion
+
+let age_In_Number= Number(age)
+
+// console.log(typeof (age_In_Number));
+
+
+
+
+let isvald= true;
+let isvald_in_number=Number(isvald)
+//console.log(isvald_in_number);
+
+
+//console.log(typeof isvald_in_number);
+
+
+let name="Dhananjay"
+
+let name_in_number=Number(name)
+// console.log(name_in_number);  //    output ->  NaN (not a number)
+
+// console.log(typeof name_in_number);
+
+
+let 
+
+
+
+
