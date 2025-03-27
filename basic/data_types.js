0c5a6d78b7df868d6b4590736fec806cd6ba1eee
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-27 14:02:29 +0000

    data_types

---
+++ basic/data_types.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newwer version
+
+let name="Dhananjay"
+let age=20
+let bigInt=12343876478687440n
+let isvalid=true
+let x
+let y=null
+
+
+console.log(typeof age);
+
+console.log(typeof null);
+
+console.log(typeof x);
+
+
+
+
+
