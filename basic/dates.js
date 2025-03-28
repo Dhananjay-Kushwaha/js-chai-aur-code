ea029ca9d25a0cb4e6341349890d2f9788fa819d
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-28 15:32:16 +0000

    dates

---
+++ basic/dates.js
@@ -0,0 +1,27 @@
+let date=new Date()
+
+
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(date.toTimeString());
+// console.log(typeof date);
+
+let time_stamp=Date.now()
+// console.log(time_stamp);
+
+// console.log(time_stamp/1000);
+// console.log(Math.round(time_stamp/1000));
+
+
+let new_date=new Date()
+
+console.log(new_date.getTime());
+console.log(new_date.getMonth()+1);
+
+
+
+
+
+
