510b7a04cee77b0754a9f8266599764d2503ee16
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-28 14:25:04 +0000

    maths

---
+++ basic/maths_n_number.js
@@ -0,0 +1,72 @@
+const score=100
+
+// console.log(score);
+
+
+const value= new Number(200)
+// console.log(value);
+
+
+
+//------
+
+// String 
+const name=score.toString()
+// console.log(name);
+
+// console.log(typeof(name));
+
+// console.log(name.length);            // length of the string 
+
+
+
+//---------
+
+const city="Kashmir"
+
+// console.log(city.toLowerCase());
+// console.log(city.toUpperCase());
+
+
+// ------
+
+const number=1000000
+// console.log(number.toLocaleString());     // Us standard 
+// console.log(number.toLocaleString('en-IN')); ///  Indian Standard
+
+
+// -----
+
+const num=123.4994
+
+// console.log(num.toPrecision(5));
+
+//----
+
+
+// console.log(Math.abs(-34));
+// console.log(Math.round(23.4));
+// console.log(Math.round(23.9));
+// console.log(Math.ceil(23.9));
+// console.log(Math.floor(23.9));
+// console.log(Math.random());
+// console.log(Math.random()+1);
+
+
+console.log(Math.sqrt(144));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
