a0d5b45497902f54e18cee099f6dc5520c298164
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-29 12:52:34 +0000

    array_02

---
+++ array/array_02.js
@@ -0,0 +1,39 @@
+const arr1=["sonu", "Monu", "him"]
+const arr2=["abs", "joker", "xyz"]
+
+
+// arr1.push(arr2)
+// console.log(arr1);
+
+// console.log(arr1[3][0]);    // output is abs --> because arr2 is treated as element in arr1 
+
+///// - - - - - - - ---- -  --  ----    ----    ----
+
+// const after_concat= arr1.concat(arr2)
+// console.log(after_concat);
+
+
+
+//   - - - -- - -   OR 
+    
+// console.log([...arr1, ...arr2]);
+
+
+
+
+
+// .from method 
+
+console.log(Array.from("Dhananjay"));
+
+
+//  .of method
+
+let a=133
+let r=1378
+let c=1
+
+// console.log(Array.of(a,r,c));
+
+
+
