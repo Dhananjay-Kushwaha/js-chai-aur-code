a74c3a074a8460a30446bc9ad8b64a4fe926ca56
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-29 11:42:31 +0000

    array

---
+++ Array/array.js
@@ -0,0 +1,41 @@
+let a=['a', "kaskd", true]
+// console.log(a[0]);
+// console.log(a[2]);
+// console.log(a[-1]);
+
+
+
+// Array Method
+a.push(34)
+a.push("hfdf")
+// console.log(a);
+
+a.pop()
+// console.log(a);
+
+// ------ - ---- - -----    ------
+let b=['a', "kaskd", true]
+const arr=b.join()  // array to string 
+
+// console.log(b);
+// console.log(arr);  /// return a string 
+
+
+//  --  --- --- --      --  --  -   -   -   
+
+let myarr=new Array(1,2,3,4,5)
+
+const arr1=myarr.slice(1,3)
+// console.log(arr1);
+// console.log(myarr);
+
+
+const arr2=myarr.splice(1,4)
+console.log(arr2);
+console.log(myarr);
+
+
+
+
+
+
