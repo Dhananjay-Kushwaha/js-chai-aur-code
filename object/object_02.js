2cdbf0f0eece1c5681c0747f5e465bba66357946
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-30 12:11:15 +0000

    object adv

---
+++ object/object_02.js
@@ -0,0 +1,70 @@
+const car= {}      
+car.name="Bugati Chiron "
+car.model="A"
+car.is_super_car="true"
+
+// console.log(car);
+
+
+/// -   -   -   -   -   -   -   -       --      Object in object
+
+const school={
+    adress: "Goa",
+    teacher_name: {
+        name :{
+                first_name:"Aman",
+                last_name:"singh", 
+        }
+    }
+}
+
+// console.log(school.adress);
+// console.log(school.teacher_name.name.first_name);
+
+
+
+// Merge in Object  
+
+const obj1={
+    A:"Sonu",
+    b:"drd",
+    name: "aman"
+
+}
+
+const obj2={
+    x:"hdbjd",
+    y: true,
+    z:0
+}
+
+// const merged =Object.assign({},obj1,obj2)       // importnace of {}
+// console.log(merged);
+
+// console.log(obj1==merged);
+
+
+//      or 
+
+const merged= {...obj1, ...obj2 }
+// console.log(merged);
+
+
+// using obj1 and obj2
+
+
+
+// console.log(Object.keys(car));          // Keys 
+// console.log(Object.values(car));           // Values
+// console.log(Object.entries(car));           // Entries
+
+console.log(obj1.hasOwnProperty("name"));
+console.log(obj1.hasOwnProperty("drd"));
+
+
+
+
+
+
+
+
