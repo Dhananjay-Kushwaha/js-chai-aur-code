fe5f1948a970c86945c1daaedefa497066c0bff7
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-30 15:43:51 +0000

    function_01

---
+++ function/function.js
@@ -0,0 +1,17 @@
+// function add(num1, num2){
+    
+    
+// }
+// add(3,9)
+
+
+function mul(a,b){
+    return a*b
+}
+
+
+// console.log(mul(8,3));  // both method is correct
+
+
+const val=(mul(2,9))
+console.log(val);
