79b7ad708d3a4792947b5b841ca7890a59eeff77
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-31 15:35:44 +0000

    Arrow function

---
+++ arrow_function/arrow_function.js
@@ -0,0 +1,25 @@
+//            1st   NORMAL FUNCTION 
+
+// const sum= function(val1, val2){
+//     return val1+val2
+// }
+// console.log(sum(3,7));
+
+
+
+//            2nd  ARROW FUNCTION
+
+// const sum= (val1, val2) =>{
+//     return val1+val2
+// }
+// console.log(sum(1,1));
+
+
+//          3rd    can be written as  (IMPLICIT RETURN)
+
+
+const sum= (val1, val2) => val1+val2
+
+console.log(sum(1,1));
+
+
