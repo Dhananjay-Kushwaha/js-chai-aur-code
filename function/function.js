09869bbaec904465a08da8678965868d34c07bef
Dhananjay <138393674+Dhananjay-Kushwaha@users.noreply.github.com>
2025-03-31 08:35:13 +0000

    adv fuct

--- function/function.js
+++ function/function.js
@@ -14,4 +14,62 @@ function mul(a,b){
 
 
 const val=(mul(2,9))
-console.log(val);
+// console.log(val);
+
+
+
+
+
+
+
+
+
+//      REST Operator  (...)
+
+
+
+function Calculate_cart_price(...val){
+    return val;
+}
+
+// console.log(Calculate_cart_price(2,4,5,6,56));  // return a array using the value
+
+
+//  Object in Function
+
+
+const car={                         // Object 
+    name:"Bugati",
+    class:"A",
+    price: "800Dollar"
+}
+
+function features(helper){                                  // Function
+    // console.log(`Car is ${helper.name} and the price is ${helper.price}` )
+}
+
+features(car)
+
+
+
+
+
+
+
+
+
+
+// Array in funtion 
+
+
+const arr=[233,55,66,77,88]                     //Array
+
+function value_at_index(getarray){              // Function
+return getarray[1]
+}
+
+
+console.log(value_at_index(arr))
+
+
+
